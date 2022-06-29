c1e6dbf28e9185cf1839ca6d1f4cb23bc83210eb
MIKE <mike.ur.addict@gmail.com>
2022-06-29 00:54:59 -0500

    ent sesion de clase

---
+++ capaEntidad/SesionDeClase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidad
+{
+    class SesionDeClase
+    {
+        public DateTime fecha { set; get; }
+        public int sesionDeclase_id { set; get; }
+        public DateTime reprogramacion { set; get; }
+        public int area_id { set; get; }
+        public int silabo_id { set; get; }
+    }
+}
